b145bd45496302d11f80d5654d8a045170a633a4
Eeresh M S <65532008+EereshM@users.noreply.github.com>
2020-07-13 16:55:50 +0530

    removed db script

--- src/main/resources/db/migration/V2__Add_people.sql
+++ src/main/resources/db/migration/V2__Add_people.sql
@@ -3,6 +3,4 @@ insert into PERSON (ID, NAME) values (2, 'Mr. Foo');
 insert into PERSON (ID, NAME) values (3, 'Ms. Bar');
 insert into PERSON (ID, NAME) values (4, 'Ms. Abc');
 insert into PERSON (ID, NAME) values (5, 'Ms. Efg');
-insert into PERSON (ID, NAME) values (6, 'Ms. Lmn');
-insert into PERSON (ID, NAME) values (7, 'Ms. Opq');
-insert into PERSON (ID, NAME) values (8, 'Ms. jghjghjgh');
+
