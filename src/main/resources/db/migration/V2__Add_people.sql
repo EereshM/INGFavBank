ea4cce763bec1b7524c97514f281af1122f85e55
Eeresh M S <65532008+EereshM@users.noreply.github.com>
2020-07-13 16:47:44 +0530

    Updated DB Migration Script

--- src/main/resources/db/migration/V2__Add_people.sql
+++ src/main/resources/db/migration/V2__Add_people.sql
@@ -1,9 +1,8 @@
 insert into PERSON (ID, NAME) values (1, 'Axel');
 insert into PERSON (ID, NAME) values (2, 'Mr. Foo');
 insert into PERSON (ID, NAME) values (3, 'Ms. Bar');
-insert into PERSON (ID, NAME) values (3, 'Ms. Abc');
-insert into PERSON (ID, NAME) values (3, 'Ms. Efg');
-
-insert into PERSON (ID, NAME) values (3, 'Ms. Lmn');
-
-insert into PERSON (ID, NAME) values (3, 'Ms. Opq');
+insert into PERSON (ID, NAME) values (4, 'Ms. Abc');
+insert into PERSON (ID, NAME) values (5, 'Ms. Efg');
+insert into PERSON (ID, NAME) values (6, 'Ms. Lmn');
+insert into PERSON (ID, NAME) values (7, 'Ms. Opq');
+insert into PERSON (ID, NAME) values (8, 'Ms. Opq');
