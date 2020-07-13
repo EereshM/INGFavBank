fe566185f49ef6f4f4860c4f0c3c512e872b2ecb
Eeresh M S <65532008+EereshM@users.noreply.github.com>
2020-07-13 16:53:10 +0530

    Updated Second Time

--- src/main/resources/db/migration/V2__Add_people.sql
+++ src/main/resources/db/migration/V2__Add_people.sql
@@ -5,4 +5,4 @@ insert into PERSON (ID, NAME) values (4, 'Ms. Abc');
 insert into PERSON (ID, NAME) values (5, 'Ms. Efg');
 insert into PERSON (ID, NAME) values (6, 'Ms. Lmn');
 insert into PERSON (ID, NAME) values (7, 'Ms. Opq');
-insert into PERSON (ID, NAME) values (8, 'Ms. Opq');
+insert into PERSON (ID, NAME) values (8, 'Ms. jghjghjgh');
